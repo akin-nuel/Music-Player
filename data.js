f094ad2d28d7fd241764365825407f031ddcea02
Akinola Emmanuel <akinolaemmanuel273@gmail.com>
2024-03-05 08:35:50 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,75 @@
+let songs = [
+    {
+        name: "Lay me down",
+        path: "songs/adele-lay.mp3",
+        artist: "Adele",
+        cover: "images/1.jpg"
+    },
+    {
+        name: "Kingdom",
+        path: "songs\GeoffreyGolden-Kingdom.mp3",
+        artist: "Geoffrey Golden",
+        cover: "images/2.jpg"
+    },
+    {
+        name: "Say you won't let go",
+        path: "songs\james-say.mp3",
+        artist: "James Arthur",
+        cover: "images/3.jpg"
+    },
+    {
+        name: "Hand of God",
+        path: "songs\jon-handofGod.mp3",
+        artist: "Jon Bellion",
+        cover: "images/4.jpg"
+    },
+    {
+        name: "Maybe IDK",
+        path: "songs\JonBellion-maybeIDK.mp3",
+        artist: "Jon Bellion",
+        cover: "images/5.jpg"
+    },
+    {
+        name: "Walk on Water",
+        path: "songs\mali-walkonwater.mp3",
+        artist: "Mali Music",
+        cover: "images/6.jpg"
+    },
+    {
+        name: "Mortal Man",
+        path: "songs\Noella-Mortal-Man.mp3",
+        artist: "Noella",
+        cover: "images/7.jpg"
+    },
+    {
+        name: "Overwhelming",
+        path: "songs\Overwhelming.mp3",
+        artist: "Jon Bellion",
+        cover: "images/8.jpg"
+    },
+    {
+        name: "Arabaribiti",
+        path: "songs\aribiti.mp3",
+        artist: "Sonnie Bandu",
+        cover: "images/9.jpg"
+    },
+    {
+        name: "Living Water",
+        path: "songs\Travis Greene-Living Water.mp3",
+        artist: "Travis Greene",
+        cover: "images/10.jpg"
+    },
+    {
+        name: "Fell in Love",
+        path: "songs\Travis-Greene-Fell.mp3",
+        artist: "Travis Greene",
+        cover: "images/11.jpg"
+    },
+    {
+        name: "Still",
+        path: "songs\Mali-Music-Still.mp3",
+        artist: "Mali Music",
+        cover: "images/12.jpg"
+    }
+
+]
